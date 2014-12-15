3d4422fe290ffbf61c85b7e7c8866e4c31699d7d
xinghuo <xinghuo@itugo.com>
2014-12-15 11:06:18 +0800

    update

---
+++ request/area/IpQueryGetRequest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * User: zhangfeng
+ * Date: 12-6-13
+ * Time: 下午1:36
+ */
+class IpQueryGetRequest{
+	private $apiParams = array();
+	function __set($key,$value){
+	  $this->apiParams[$key] = $value;
+	}
+	function __get($key){
+        return  $this->apiParams[$key];
+	}
+    public function getAppJsonParams()
+    {
+        
+        ksort($this->apiParams);
+        return json_encode($this->apiParams);
+    }
+	    /**
+     *
+     * 获取方法名称
+     * @return string
+     */
+    public function getApiMethod()
+    {
+        return "jingdong.area.ip.query";
+    }
+}
+ 
